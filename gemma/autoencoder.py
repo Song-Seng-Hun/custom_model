78c3fdf8e1e631083e838d7483732d4917675abc
송승훈 <69943723+Song-Seng-Hun@users.noreply.github.com>
2024-02-27 10:31:27 +0900

    Seperate Text generator

--- gemma/autoencoder.py
+++ gemma/autoencoder.py
@@ -32,6 +32,101 @@ class TextDecoder(nn.Module):
             top_ks=top_ks,
         )
         return next_tokens
+    
+class TextPatchfier(nn.Module):
+    def __init__(self):
+        super(TextPatchfier, self).__init__()
+
+    def forward(self, prompts, tokenizer, output_len, max_position_embeddings):
+        is_str_prompt = isinstance(prompts, str)        
+        if is_str_prompt:
+            prompts = [prompts]
+
+        batch_size = len(prompts)
+        prompt_tokens = [tokenizer.encode(prompt) for prompt in prompts]
+        min_prompt_len = min(len(p) for p in prompt_tokens)
+        max_prompt_len = max(len(p) for p in prompt_tokens)
+        max_seq_len = max_prompt_len + output_len
+        assert max_seq_len <= max_position_embeddings
+        return is_str_prompt, prompts, batch_size, prompt_tokens, min_prompt_len, max_prompt_len, max_seq_len
+
+
+class TextGenerator(nn.Module):
+    def __init__(self):
+        super(TextGenerator, self).__init__()
+    def forward(self, forward, batch_size, max_seq_len, tokenizer, min_prompt_len, prompt_tokens, device, temperature, top_p, top_k, kv_caches, output_len, is_str_prompt):
+        token_ids_tensor = torch.full((batch_size, max_seq_len),
+                                            tokenizer.pad_id, dtype=torch.int64)
+        input_token_ids_tensor = torch.full((batch_size, min_prompt_len),
+                                            tokenizer.pad_id,
+                                            dtype=torch.int64)
+        for i, p in enumerate(prompt_tokens):
+            token_ids_tensor[i, :len(p)] = torch.tensor(p)
+            input_token_ids_tensor[i, :min_prompt_len] = torch.tensor(
+                p[:min_prompt_len])
+        token_ids_tensor = token_ids_tensor.to(device)
+        input_token_ids_tensor = input_token_ids_tensor.to(device)
+        prompt_mask_tensor = token_ids_tensor != tokenizer.pad_id # 들어있는것만 마스킹
+        input_positions_tensor = torch.arange(0, min_prompt_len,
+                                            dtype=torch.int64).to(device)
+        mask_tensor = torch.full((1, 1, max_seq_len, max_seq_len),
+                                -2.3819763e38).to(torch.float)
+        mask_tensor = torch.triu(mask_tensor, diagonal=1).to(device)
+        curr_mask_tensor = mask_tensor.index_select(2, input_positions_tensor)
+        output_positions_tensor = torch.LongTensor([min_prompt_len - 1]).to(
+            device)
+        temperatures_tensor = torch.FloatTensor([temperature] * batch_size).to(
+            device)
+        top_ps_tensor = torch.FloatTensor([top_p] * batch_size).to(device)
+        top_ks_tensor = torch.LongTensor([top_k] * batch_size).to(device)
+        output_index = torch.tensor(min_prompt_len, dtype=torch.int64).to(
+            device)
+
+        # Prefill up to min_prompt_len tokens, then treat other prefill as
+        # decode and ignore output.
+        for i in range(max_seq_len - min_prompt_len):
+            next_token_ids = forward(
+                input_token_ids=input_token_ids_tensor,
+                input_positions=input_positions_tensor,
+                kv_write_indices=None,
+                kv_caches=kv_caches,
+                mask=curr_mask_tensor,
+                output_positions=output_positions_tensor,
+                temperatures=temperatures_tensor,
+                top_ps=top_ps_tensor,
+                top_ks=top_ks_tensor,
+            )
+
+            curr_prompt_mask = prompt_mask_tensor.index_select(
+                1, output_index).squeeze(dim=1)
+            curr_token_ids = token_ids_tensor.index_select(
+                1, output_index).squeeze(dim=1)
+            output_token_ids = torch.where(curr_prompt_mask, curr_token_ids,
+                                        next_token_ids).unsqueeze(dim=1)
+            token_ids_tensor.index_copy_(1, output_index, output_token_ids)
+
+            input_token_ids_tensor = output_token_ids
+            input_positions_tensor = output_index.unsqueeze(dim=-1)
+            curr_mask_tensor = mask_tensor.index_select(2,
+                                                        input_positions_tensor)
+            output_positions_tensor = torch.tensor(0, dtype=torch.int64).to(
+                device)
+            output_index = output_index + 1
+        # Detokenization.
+        token_ids = token_ids_tensor.tolist()
+        text_results = []
+        for i, tokens in enumerate(token_ids):
+            trimmed_output = tokens[len(prompt_tokens[i]):len(prompt_tokens[i])
+                                    + output_len]
+            ## 종료 조건 -> 잘라내기
+            if tokenizer.eos_id in trimmed_output:
+                eos_index = trimmed_output.index(tokenizer.eos_id)
+                trimmed_output = trimmed_output[:eos_index]
+            text_results.append(tokenizer.decode(trimmed_output))
+        # If a string was provided as input, return a string as output.
+        text_results = text_results[0] if is_str_prompt else text_results
+        return text_results
+
 
 class Encoder1d(nn.Module):
     def __init__(self, resize=None, patch_size=128, embed_dim=2048, model_path=None):
